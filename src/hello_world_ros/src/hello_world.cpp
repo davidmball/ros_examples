a3d374d6a0443fbec2358c9c3f10f3bb242b9c04
davidmball <davidmichaelball@gmail.com>
2017-08-13 01:56:02 +0200

    Added stream to the hellow world example

--- src/hello_world_ros/src/hello_world.cpp
+++ src/hello_world_ros/src/hello_world.cpp
@@ -8,10 +8,15 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "hello_world");
   ros::NodeHandle n;
 
-  ROS_DEBUG("Hello world! (debug)"); // won't appear
+  ROS_DEBUG("Hello world! (debug)"); // won't appear due to default log level
   ROS_INFO("Hello world! (info)");
   ROS_WARN("Hello world! (warn)");
   ROS_ERROR("Hello world! (error)");
+
+  ROS_DEBUG_STREAM("Hello world! " << "(debug)"); // won't appear due to default log level
+  ROS_INFO_STREAM("Hello world! " << "(info)");
+  ROS_WARN_STREAM("Hello world! " << "(warn)");
+  ROS_ERROR_STREAM("Hello world! " << "(error)");
 
   return EXIT_SUCCESS;
 }
