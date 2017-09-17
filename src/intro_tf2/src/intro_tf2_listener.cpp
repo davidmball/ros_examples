d49c2f427b71ddf971545a6742e2ca00c4297a01
Steffen Fuchs <st.fuchs.tr@gmail.com>
2017-09-17 16:43:05 +0200

    added python version

--- src/intro_tf2/src/intro_tf2_listener.cpp
+++ src/intro_tf2/src/intro_tf2_listener.cpp
@@ -5,30 +5,24 @@
 class ListenerNode
 {
 public:
-  ListenerNode() {}
+  ListenerNode()
+    : source_frame_("base_link") //
+    , target_frame_("map")
+  { }
 
-  bool init(const ros::NodeHandle& nh_priv = ros::NodeHandle("~"))
+  bool configure()
   {
     tf2_buffer_.reset(new tf2_ros::Buffer);
     tf2_listener_.reset(new tf2_ros::TransformListener(*tf2_buffer_));
-    nh_priv.param<std::string>("robot_frame_id", source_frame_, "base_link");
-    nh_priv.param<std::string>("world_frame_id", target_frame_, "map");
-    return true;
-  }
-
-  bool cleanup()
-  {
-    tf2_listener_.reset();
-    tf2_buffer_.reset();
     return true;
   }
 
   void get_robot_pose(const ros::Time& stamp = ros::Time(0))
   {
-    tf2::Transform tf; // Transform class is good for calculating stuff
+    tf2::Transform tf; // useful if you want to work with your transform
     try
     {
-      tf2::fromMsg(
+      tf2::fromMsg( // convert geometry_msgs::Transform to tf2::Transform
         tf2_buffer_->lookupTransform(
           target_frame_,
           source_frame_,
@@ -40,8 +34,9 @@ public:
       ROS_WARN_THROTTLE(1, "%s", ex.what());
       return;
     }
-    tf2::Vector3 origin = tf * tf2::Vector3(0,0,0);
-    ROS_INFO_STREAM(origin.x() << " " << origin.y() << " " << origin.z());
+    ROS_INFO_STREAM(tf.getOrigin().x() << " " <<
+                    tf.getOrigin().y() << " " <<
+                    tf.getOrigin().z());
   }
 
   void spin()
@@ -61,10 +56,11 @@ private:
   std::string source_frame_, target_frame_;
 };
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "tf2_listener_node");
   ListenerNode node;
-  node.init();
+  node.configure();
   node.spin();
 
   return EXIT_SUCCESS;
