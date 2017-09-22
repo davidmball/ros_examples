c5ee7a4de5a61c4ffc0e0c36f3a62728220be3e8
davidmball <davidmichaelball@gmail.com>
2017-09-22 16:16:10 +0200

    Added ros tutorial timers example.

---
+++ src/timers/timers.cpp
@@ -0,0 +1,33 @@
+/* From https://github.com/ros/ros_tutorials. See LICENSE file for license information. */
+#include "ros/ros.h"
+
+/**
+ * This tutorial demonstrates the use of timer callbacks.
+ */
+
+void callback1(const ros::TimerEvent&)
+{
+  ROS_INFO("Callback 1 triggered");
+}
+
+void callback2(const ros::TimerEvent&)
+{
+  ROS_INFO("Callback 2 triggered");
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+
+  /**
+   * Timers allow you to get a callback at a specified rate.  Here we create
+   * two timers at different rates as a demonstration.
+   */
+  ros::Timer timer1 = n.createTimer(ros::Duration(0.1), callback1);
+  ros::Timer timer2 = n.createTimer(ros::Duration(1.0), callback2);
+
+  ros::spin();
+
+  return 0;
+}
